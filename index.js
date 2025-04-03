5143b835356bc818802bfb89a7fb2a28ac8d15ea
Matt Walsh <github@netbymatt.com>
2025-04-03 14:39:46 -0500

    update dependencies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // launch express to serve files
 const express = require('express');
 const path = require('path');
+const apiPassthru = require('./apipassthru');
 
 const PORT = 3000;
 
@@ -8,16 +9,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use('/resources/dev/flot', express.static('flot/source'));
 app.use('/dist', express.static('dist'));
-app.use('/*', (req, res, next) => {
-	if (req.originalUrl === '/') {
-		res.render(path.join(__dirname, './html/index'), { version: null, _apiUrl: `http://localhost:${PORT}/` });
-	} else {
-		next();
-	}
-});
-app.use('/', express.static(path.join(__dirname, './html')));
-
-const apiPassthru = require('./apipassthru');
+app.use('/resources', express.static('./html/resources'));
+app.use('/images', express.static('./html/images'));
 
 // pass through to api
 app.use('/points', apiPassthru);
@@ -25,5 +18,15 @@ app.use('/gridpoints', apiPassthru);
 app.use('/stations', apiPassthru);
 app.use('/alerts', apiPassthru);
 app.use('/products', apiPassthru);
+
+// root document
+app.use('/', (req, res, next) => {
+	if (req.originalUrl === '/') {
+		res.render(path.join(__dirname, './html/index'), { version: null, _apiUrl: `http://localhost:${PORT}/` });
+	} else {
+		next();
+	}
+});
+app.use('/*url', express.static(path.join(__dirname, './html')));
 
 app.listen(PORT);
